4f968045eb7fe8ef4b193f409feb3dc6aab1437f
Ramu Narasinga <ramu199611@gmail.com>
2020-02-14 22:40:03 +0530

    added cors issue changes

--- functions/chrome/chrome.js
+++ functions/chrome/chrome.js
@@ -9,8 +9,8 @@ exports.handler = async (event, context, callback) => {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Max-Age': '2592000',
+    // 'Access-Control-Allow-Methods': '*',
+    "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
     'Access-Control-Allow-Credentials': 'true',
   }
   try {
